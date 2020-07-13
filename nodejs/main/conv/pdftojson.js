1221c3fd9a783596d519d8b35b18757b6a5eac84
LEECHUNGYOON <36251628+LEECHUNGYOON@users.noreply.github.com>
2020-07-14 00:37:55 +0900

    Update pdftojson.js
    promise 사용한 분산처리..
    집에서 보완..
    
    2020-07-14

--- nodejs/main/conv/pdftojson.js
+++ nodejs/main/conv/pdftojson.js
@@ -10,31 +10,63 @@ let express = require('express'),
 		- URL Path별, GET/POST 별 callback function 지정
 */ 
 
-router.post('/pdfmulti', f_pdfmulty);
+router.post('/pdfjson', f_pdftojson);
 
 /* 
 	## callback Funtions 
 		- Url Path 별 callback function
 */
 
-function f_pdfmulty(req, res, next){
-	debugger;
+function f_pdftojson(req, res, next){
 
 	let oBody = req.body,
 		sFile = oBody.files,
 		aFiles = JSON.parse(sFile),
 		iFileLen = aFiles.length,
-		oJsonData = [];
+		aPromise = [],
+		aJsonData = [];
 		
 	if(iFileLen == 0){
 		f_return('E', 'File not Exists!');
 		return;
 	}
+	
+	debugger;
 
 	for(var i = 0; i < iFileLen; i++){
 		let oFile = aFiles[i];
-		f_ParsePDFtoJson(oFile, iFileLen);
+
+		var oPromi = new Promise(function (resolve, reject){
+			f_ParsePDFtoJson(oFile, iFileLen, resolve, reject);
+		});
+
+		aPromise.push(oPromi);
+
 	}
+
+	Promise.all(aPromise).then(function(value){
+		debugger;
+
+		var returnJson = JSON.stringify(value);
+
+		res.status(200);
+		res.setHeader('Content-Type', 'application/json');
+		res.json(returnJson);
+
+		//응답 종료
+    	res.end(); 
+
+	})
+	.catch(function(errData){
+		debugger;
+
+		f_return('E', 'PDF Parse Error!!');
+
+		//응답 종료
+    	res.end(); 
+
+		//reject(errData);
+	});
 
     /*
     res.status(200);
@@ -47,26 +79,37 @@ function f_pdfmulty(req, res, next){
 
 }
 
-function f_ParsePDFtoJson(oFile, iFileLen){
+function f_ParsePDFtoJson(oFile, iFileLen, resolve, reject){
 
 	let pdfParse = new pdf2json();
-		//oJsonData = [];
 
 	let buf = Buffer.from(oFile.CONTENT, 'hex');
 
 	pdfParse.parseBuffer(buf);
 
 	pdfParse.on("pdfParser_dataError", function(errData){ 
-    	f_return('E', 'PDF Parsing Error!!');     	
+
+    	reject(errData); // promise error
+    	f_return('E', 'PDF Parsing Error!!');   // error response
     	return;
     });
 
     pdfParse.on("pdfParser_dataReady", function(pdfData){ 	
-    	debugger;
-
+   		
     	let ojson = JSON.stringify(pdfData.formImage.Pages);
     	let decode = decodeURIComponent(ojson);
 
+    	let convFile = oFile;
+    	convFile.CONVJSON = decode;
+    	if(convFile.CONTENT){
+    		delete convFile.CONTENT;
+    	}
+
+    	resolve(convFile);
+
+    	//return decode;
+
+    	/*
     	oJsonData.push(decode);
 
     	if(oJsonData.length == iFileLen){
@@ -78,7 +121,7 @@ function f_ParsePDFtoJson(oFile, iFileLen){
 		    res.end(); 
     		return;
     	}
-
+		*/
     });    
 }
 
